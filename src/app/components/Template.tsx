6bc6071a5e105591dcb3a67f552041f062d2b0cf
Alexander <alexmenshikov7@gmail.com>
2025-04-11 17:12:49 +0500

    update template styles

--- src/app/components/Template.tsx
+++ src/app/components/Template.tsx
@@ -1,9 +1,11 @@
-const Template = ({children}: Readonly<{
-    children: React.ReactNode;
-  }>) => {
-    return(
-        <div className="flex w-[70vw]">{children}</div>
-    )
-}
+const Template = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="flex w-[70vw] flex-wrap justify-center">{children}</div>
+  );
+};
 
-export default Template
+export default Template;
