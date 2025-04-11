94b6bd11063d37d316965dedb12a18bae633e39d
Alexander <alexmenshikov7@gmail.com>
2025-04-11 17:10:36 +0500

    added first and second block components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavigationPanel from './components/NavigationPanel';
 import Template from './components/Template';
+import FirstBlock from './components/FirstBlock';
+import SecondBlock from './components/SecondBlock';
 
- const MainPage = () => {
+const MainPage = () => {
   return (
     <Template>
-    <NavigationPanel/>
+      <NavigationPanel />
+      <FirstBlock />
+      <SecondBlock />
     </Template>
   );
-}
+};
 
-export default MainPage
+export default MainPage;
