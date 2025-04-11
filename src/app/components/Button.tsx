0d1f848922d4084b97748c9716775214406b8aab
Alexander <alexmenshikov7@gmail.com>
2025-04-11 17:10:47 +0500

    added button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,14 @@
+
+interface IButtonProps {
+    className:string;
+    text:string;
+}
+
+
+const Button: React.FC<IButtonProps> = ({className, text}) => {
+    return <button className={className}>
+          {text}
+        </button>
+}
+
+export default Button
