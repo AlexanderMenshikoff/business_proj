2cadc1635f2ca78b744378e3011ba3ee30cb9411
Alexander <alexmenshikov7@gmail.com>
2025-04-13 20:16:43 +0500

    added new components on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,12 @@ import NavigationPanel from './components/NavigationPanel';
 import Template from './components/Template';
 import FirstBlock from './components/FirstBlock';
 import SecondBlock from './components/SecondBlock';
+import ThirdBlock from './components/ThirdBlock';
+import FourthBlock from './components/FourthBlock';
+import CustomerBlock from './components/CustomerBlock';
+import FAQBlock from './components/FAQBlock';
+import AlmostEndBlock from './components/AlmostEndBlock';
+import Footer from './components/Footer';
 
 const MainPage = () => {
   return (
@@ -10,7 +16,14 @@ const MainPage = () => {
       <NavigationPanel />
       <FirstBlock />
       <SecondBlock />
+      <ThirdBlock/>
+      <FourthBlock/>
+      <CustomerBlock/>
+      <FAQBlock/>
+      <AlmostEndBlock/>
+      <Footer/>
     </Template>
+    
   );
 };
 
