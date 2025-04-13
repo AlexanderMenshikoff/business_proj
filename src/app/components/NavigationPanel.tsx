0c259afc6aba1786db43e27f7a37311c27f8b9cb
Alexander <alexmenshikov7@gmail.com>
2025-04-13 20:20:39 +0500

    changed some sizes for text and buttons

--- src/app/components/NavigationPanel.tsx
+++ src/app/components/NavigationPanel.tsx
@@ -4,26 +4,34 @@ import Button from './Button';
 const NavigationPanel = () => {
   return (
     <nav className="flex w-[100vw] justify-between items-center mt-[10px]">
-      <div className="flex gap-15">
+      <div className="flex gap-10">
         <Image src="/logo.png" height={50} width={100} alt="Logo" />
-        <ul className="flex gap-9 items-center">
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
+        <ul className="flex gap-8 items-center">
+          <li className="text-[12px]">
+            <a>Features</a>
+          </li>
+          <li className="text-[12px]">
+            <a>Pricing</a>
+          </li>
+          <li className="text-[12px]">
+            <a>About</a>
+          </li>
+          <li className="text-[12px]">
+            <a>Contact</a>
+          </li>
         </ul>
       </div>
 
-      <div className="flex gap-9">
+      <div className="flex gap-3">
         <Button
           className={
-            'rounded-[5px] bg-[#232524] py-[10px] px-[25px] cursor-pointer'
+            'rounded-[5px] bg-[#232524] py-[8px] px-[15px] cursor-pointer text-[12px] font-bold'
           }
           text={'Sign in'}
         />
         <Button
           className={
-            'rounded-[5px] bg-[#ffc44e] py-[10px] px-[25px] cursor-pointer text-black'
+            'rounded-[5px] bg-[#ffc44e] py-[8px] px-[15px] cursor-pointer text-black text-[12px] font-bold'
           }
           text={'Get started'}
         />
