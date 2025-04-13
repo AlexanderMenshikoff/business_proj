8e23b22616d6c2a8aa9bf359e9c3a7b90becfbd0
Alexander <alexmenshikov7@gmail.com>
2025-04-13 20:19:38 +0500

    created brand new Footer block component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import { FooterLinksData } from '../data';
+
+const Footer = () => {
+  return (
+    <div className="w-[100vw] mb-[100px] mt-[20px]">
+      <div className="flex justify-between mt-[70px] flex-wrap mb-[60px]">
+        {FooterLinksData.map((el) => (
+          <div key={el.id} className="">
+            <p className="mb-[15px] text-[#b6b6b6] text-[12px]">
+              {el.columnName}
+            </p>
+            <div className="">
+              {el.columnText.map((link) => (
+                <a
+                  key={el.id}
+                  className="block cursor-pointer mb-[15px] text-[12px]"
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <div className="border-[1px] border-[#1b1b1b] mb-[20px] "></div>
+        <div className="flex justify-between items-center">
+          <Image src="/logo.png" alt="" width={100} height={100} />
+          <p className="text-[12px] text-[#b6b6b6]">© Torch 2024</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
