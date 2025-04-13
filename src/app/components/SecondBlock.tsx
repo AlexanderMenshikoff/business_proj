2801977c811ecab96539b9ef5fda3d015390e315
Alexander <alexmenshikov7@gmail.com>
2025-04-13 20:21:03 +0500

    changed some sizies, used Prettier

--- src/app/components/SecondBlock.tsx
+++ src/app/components/SecondBlock.tsx
@@ -3,22 +3,27 @@ import { SecondBlockData } from '../data';
 
 const SecondBlock = () => {
   return (
-    <div>
-      <h2 className='text-[40px] mb-[25px]'>Take action from insights</h2>
+    <div className="mb-[100px] mt-[70px]">
+      <h2 className="text-[40px] mb-[50px]">Take action from insights</h2>
       <div className="grid grid-cols-4 grid-rows-5 gap-4">
-        {SecondBlockData.map(el => 
-        <div className={`${el.className} bg-[#171918] pl-[25px] relative content-end`} key={el.id}>
-          
-            <Image className='absolute top-[20px] left-[20px]' src={el.img} width={el.imgWidth} height={30} alt='block img'/>
-            <div className='mt-[60px] mb-[20px]'>
-            <p className='text-[25px] '>{el.header}</p>
-            <p className='text-[15px]'>{el.text}</p> 
+        {SecondBlockData.map((el) => (
+          <div
+            className={`${el.className} bg-[#171918] pl-[25px] relative content-end rounded-[10px]`}
+            key={el.id}
+          >
+            <Image
+              className="absolute top-[20px] left-[20px]"
+              src={el.img}
+              width={el.imgWidth}
+              height={30}
+              alt="block img"
+            />
+            <div className="mt-[60px] mb-[20px] pr-[20px]">
+              <p className="text-[23px] mb-[10px]">{el.header}</p>
+              <p className="text-[12px] text-[#b6b6b6]">{el.text}</p>
             </div>
-            
-           
-        </div>
-        )}
-        
+          </div>
+        ))}
       </div>
     </div>
   );
