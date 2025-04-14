53b3e8d9afd98597e02d6d834edd5ada9a40791b
Alexander <alexmenshikov7@gmail.com>
2025-04-15 01:12:29 +0500

    added cursor pointer

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-const Logo = () => <Image src="/logo.png" height={50} width={100} alt="Logo" />
+const Logo = () => <Image src="/logo.png" height={50} width={100} alt="Logo" className="cursor-pointer"/>
 
 
 export default Logo
